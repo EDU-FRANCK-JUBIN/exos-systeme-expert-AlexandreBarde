d061b36e856e1790e1f2a9a25a8e92dc5a182c49
alexandre.barde@thalesaleniaspace.com <alexandre.barde@thalesaleniaspace.com>
2019-11-29 13:32:24 +0100

    init

---
+++ Grenouilles/main.py
@@ -0,0 +1,17 @@
+from pyDatalog import pyDatalog
+
+pyDatalog.clear()
+
+pyDatalog.create_terms('croakes,eatflies,frog,chirps,sings,canary,green,jaune,P,X,Variable')
+
+frog(P)  <= croakes(P) & eatflies(P)
+canary(P) <= chirps(P) & sings(P)
+green(P) <= frog(P)
+jaune(P) <= canary(P)
+
+pyDatalog.assert_fact('croakes', 'fritz')
+pyDatalog.assert_fact('eatflies', 'fritz')
+
+print(pyDatalog.ask('green(X)'))
+print(pyDatalog.ask('green(fritz)'))
+print(pyDatalog.ask('green(xyz)'))
